9697b644c4f9a93b296499dcf3a269b8aca70e00
chdmtm@gmail.com <chdmtm@gmail.com>
2022-04-12 16:54:39 +0700

    create base spring boot classes and properties

---
+++ src/main/java/com/example/expasoft/ExpaSoftApplication.java
@@ -0,0 +1,12 @@
+package com.example.expasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpaSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpaSoftApplication.class, args);
+    }
+}
